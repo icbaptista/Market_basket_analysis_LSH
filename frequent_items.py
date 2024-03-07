92824b3dd2eab260ac1bf285c363f10b36db81e8
icbaptista <ines.castro.batista@gmail.com>
2024-03-07 11:54:27 +0000

    changing SQL operations to RDD transformations

--- frequent_items.py
+++ frequent_items.py
@@ -1,17 +1,19 @@
+from pyspark import SparkContext
 from pyspark.sql import SparkSession
-import sys 
 from pyspark.sql import SQLContext
+import sys
 
 if __name__ == '__main__':
-
     if len(sys.argv) != 3:
         print("Usage: script.py <input_file> <output_directory>")
         exit(1)
-    
+
+    sc = SparkContext(appName="FrequentItems")
+
     sc_sql = SparkSession.builder \
         .appName("Example") \
         .getOrCreate()
-    
+
     sqlContext = SQLContext(sc_sql)
 
     logData = sc_sql.read.option("header", "true").csv(sys.argv[1])
@@ -19,19 +21,25 @@ if __name__ == '__main__':
     # Count the number of rows
     print("Number of rows: ", logData.count())
 
-    # Reorder the data by the patient - column Patient
-    logData.createOrReplaceTempView("logData")
-    logData = sqlContext.sql("SELECT * FROM logData ORDER BY Patient")
+    # Show the schema
+    logData.printSchema()
+
+    # Transform the data into a RDD
+    logData = logData.rdd
+
+    # Eliminate the columns that are not needed - columns START, STOP, ENCOUNTER and DESCRIPTION
+    # Only keep columns PATIENT and CODE
+    logData = logData.map(lambda x: (x[2], x[4]))
 
     # Show the data
-    # logData.show()
+    print(logData.take(5))
 
-    # Get the frequencies of the elements in the column Code 
-    freq = logData.groupBy("Code").count()
-    freq.show()
+    # Reorder the data by the patient - column Patient
+    logData = logData.sortBy(lambda x: x[0]) 
 
+    # Get the frequencies of the elements in the column Code
+    freq = logData.map(lambda x: (x[1], 1)).reduceByKey(lambda a, b: a + b)
 
-
-
-
+    # Discard elements from column code with frequency less than 500 from logData
+    logData = logData.filter(lambda x: freq[x[1]] > 500)
 

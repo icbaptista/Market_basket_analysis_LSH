c227a6c82e261b0c724cdbcb8252eb0ab2d8c7a4
jteles277 <jteles277@gmail.com>
2024-04-08 23:33:43 +0100

    LSH code

---
+++ ex2/lsh_get_sim.py
@@ -0,0 +1,219 @@
+import pandas as pd
+import numpy as np 
+
+from itertools import combinations
+
+import argparse
+
+# Create the parser
+parser = argparse.ArgumentParser(description='Process some integers.')
+
+# Add the arguments
+parser.add_argument('-b', type=int, required=True, help='The value for b')
+parser.add_argument('-r', type=int, required=True, help='The value for r')
+
+# Parse the arguments
+args = parser.parse_args()
+ 
+ 
+class LSH:
+    buckets = []
+    counter = 0
+    def __init__(self, b, r):
+        self.b = b
+        self.r = r
+        for i in range(b):
+            self.buckets.append({})
+
+    def make_subvecs(self, signature):
+        l = len(signature) 
+        assert l % self.b == 0
+        r = self.r
+        # break signature into subvectors
+        subvecs = []
+        for i in range(0, l, r):
+            subvecs.append(signature[i:i+r])
+        return np.stack(subvecs)
+    
+    def add_hash(self, signature):
+        subvecs = self.make_subvecs(signature).astype(str)
+        for i, subvec in enumerate(subvecs):
+            subvec = ','.join(subvec)
+            if subvec not in self.buckets[i].keys():
+                self.buckets[i][subvec] = []
+            self.buckets[i][subvec].append(self.counter)
+        self.counter += 1
+
+    def check_candidates(self):
+        candidates = []
+        for bucket_band in self.buckets:
+            keys = bucket_band.keys()
+            for bucket in keys:
+                hits = bucket_band[bucket]
+                if len(hits) > 1:
+                    candidates.extend(combinations(hits, 2))
+        return set(candidates)
+
+# Getting Data 
+
+filepath = "covid_news_small.json"
+
+data = pd.read_json(filepath, lines=True)
+data = data["text"].to_list() 
+articles = data[0:1000] 
+
+import requests
+import pandas as pd
+import numpy as np
+import io
+
+url = "https://raw.githubusercontent.com/brmson/dataset-sts/master/data/sts/sick2014/SICK_train.txt"
+
+text = requests.get(url).text
+
+data = pd.read_csv(io.StringIO(text), sep='\t')
+data.head()
+
+sentences = data['sentence_A'].tolist() 
+
+# Shingling
+
+k = 8  # shingle size
+
+def build_shingles(sentence: str, k: int):
+    shingles = []
+    for i in range(len(sentence) - k):
+        shingles.append(sentence[i:i+k])
+    return set(shingles)
+
+def build_vocab(shingle_sets: list):
+    # convert list of shingle sets into single set
+    full_set = {item for set_ in shingle_sets for item in set_}
+    vocab = {}
+    for i, shingle in enumerate(list(full_set)):
+        vocab[shingle] = i
+    return vocab
+
+def one_hot(shingles: set, vocab: dict):
+    vector = np.zeros(len(vocab))
+    for shingle in shingles:
+        if shingle in vocab:
+            vector[vocab[shingle]] = 1
+        elif shingle == 'special':
+            vector[-1] = 1  # Use the last entry for the special element
+    return vector
+
+
+print("Building Shingles...")
+# build shingles
+shingles = []
+for sentence in articles:
+    shingles.append(build_shingles(sentence, k))
+    
+print("Building Vocab...")
+# build vocab
+vocab = build_vocab(shingles)
+
+print("One-Hot Encoding...")
+# one-hot encode our shingles
+shingles_1hot = []
+for shingle_set in shingles:
+    if not any(shingle_set):
+        shingle_set.add('special')  # Add a special element
+    shingles_1hot.append(one_hot(shingle_set, vocab))
+# stack into single numpy array
+shingles_1hot = np.stack(shingles_1hot) 
+
+
+
+# Min Hashing
+print("Min Hashing...")
+def minhash_arr(vocab: dict, resolution: int):
+    length = len(vocab.keys())
+    arr = np.zeros((resolution, length))
+    for i in range(resolution):
+        permutation = np.random.permutation(len(vocab)) + 1
+        arr[i, :] = permutation.copy() 
+    return arr.astype(int)
+
+def get_signature(minhash, vector):
+    # get index locations of every 1 value in vector
+    idx = np.nonzero(vector)[0].tolist()
+    # use index locations to pull only +ve positions in minhash
+    shingles = minhash[:, idx]
+    
+    # find minimum 3 in each hash vector 
+    signature = np.min(shingles, axis=1) 
+    return signature
+
+arr = minhash_arr(vocab, 200)
+
+signatures = []
+
+for vector in shingles_1hot:
+    signatures.append(get_signature(arr, vector))
+
+# merge signatures into single array
+signatures = np.stack(signatures) 
+
+
+# LSH 
+
+print("Locality-Sensitive Hashing (LSH)...")
+
+b = args.b 
+r = args.r
+
+lsh = LSH(b, r)
+
+for signature in signatures:
+    lsh.add_hash(signature)
+     
+    
+candidate_pairs = lsh.check_candidates() 
+
+
+# Ploting
+
+print("Getting the one...")
+
+
+
+# Function to calculate Jaccard similarity
+def jaccard_similarity(set1, set2):
+    intersection = len(set1.intersection(set2))
+    union = len(set1.union(set2))
+    return intersection / union if union != 0 else 0
+ 
+ 
+def find_similar_articles(article):
+    article_shingles = build_shingles(article, k)
+    similar_articles = []
+
+    for pair in candidate_pairs:
+        x, y = pair 
+        if article == articles[x] or article == articles[y]:
+            
+            other_article = articles[y] if article == articles[x] else articles[x]
+            
+            vector_x = articles[x]
+            vector_y = articles[y]    
+            
+            sim = jaccard_similarity(set(vector_x), set(vector_y)) 
+
+            if sim < 1.0 and sim > 0.85:
+                similar_articles.append(other_article)
+            
+             
+
+    return similar_articles
+
+
+for i in range(0, 100):
+    results = find_similar_articles(articles[i])
+    if len(results) > 0:
+        print("article being searched: ", articles[i])
+        print("\n\n")
+        print("similar articles found: ", find_similar_articles(articles[i]))
+        print("\n\n")
+ 
--- ex2/run_lsh.py
+++ ex2/run_lsh.py
@@ -1,34 +1,15 @@
 import pandas as pd
 import numpy as np 
+from plot import plot_lsh
 from lsh import *
 from itertools import combinations
-
 import argparse
+import math
+from sklearn.metrics.pairwise import cosine_similarity
 
-# Create the parser
-parser = argparse.ArgumentParser(description='Process some integers.')
-
-# Add the arguments
-parser.add_argument('-b', type=int, required=True, help='The value for b')
-parser.add_argument('-r', type=int, required=True, help='The value for r')
-
-# Parse the arguments
-args = parser.parse_args()
  
-n_hashes = 200 
- 
-# Getting Data 
 
-filepath = "covid_news_small.json"
-
-data = pd.read_json(filepath, lines=True)
-data = data["text"].to_list() 
-articles = data[0:1000]  
-
-# Shingling
-
-k = 8  # shingle size
-
+# Shingling 
 def build_shingles(sentence: str, k: int):
     shingles = []
     for i in range(len(sentence) - k):
@@ -52,31 +33,9 @@ def one_hot(shingles: set, vocab: dict):
             vector[-1] = 1  # Use the last entry for the special element
     return vector
 
+ 
 
-print("Building Shingles...")
-# build shingles
-shingles = []
-for sentence in articles:
-    shingles.append(build_shingles(sentence, k))
-    
-print("Building Vocab...")
-# build vocab
-vocab = build_vocab(shingles)
-
-print("One-Hot Encoding...")
-# one-hot encode our shingles
-shingles_1hot = []
-for shingle_set in shingles:
-    if not any(shingle_set):
-        shingle_set.add('special')  # Add a special element
-    shingles_1hot.append(one_hot(shingle_set, vocab))
-# stack into single numpy array
-shingles_1hot = np.stack(shingles_1hot) 
-
-
-
-# Min Hashing
-print("Min Hashing...")
+# Min Hashing 
 def minhash_arr(vocab: dict, resolution: int):
     length = len(vocab.keys())
     arr = np.zeros((resolution, length))
@@ -94,108 +53,24 @@ def get_signature(minhash, vector):
     # find minimum 3 in each hash vector 
     signature = np.min(shingles, axis=1) 
     return signature
-
-arr = minhash_arr(vocab, n_hashes)
-
-signatures = []
-
-for vector in shingles_1hot:
-    signatures.append(get_signature(arr, vector))
-
-# merge signatures into single array
-signatures = np.stack(signatures) 
-
-
-# LSH 
-
-print("Locality-Sensitive Hashing (LSH)...")
-
-b = args.b 
-r = args.r
-
-lsh = LSH(b, r)
-
-for signature in signatures:
-    lsh.add_hash(signature)
-     
-    
-candidate_pairs = lsh.check_candidates() 
-
-# Ploting
-
-print("Plotting...")
-
-from sklearn.metrics.pairwise import cosine_similarity
-import matplotlib.pyplot as plt 
- 
  
 
-def jaccard(a: set, b: set):
-    return len(a.intersection(b)) / len(a.union(b))
+# Evaluation
 
-pairs = pd.DataFrame({
-    'x': [],
-    'y': [],
-    'jaccard': [],
-    'cosine': [],
-    'candidate': []
-})
-
-x_= []
-y_= []
-jaccard_ = []
-cosine_ = []
-candidate_ = [] 
-
-data_len = shingles_1hot.shape[0]
-chosen = set()
-# take random sample of pairs
-sample_size = 50_000
-for _ in range(sample_size):
+# Function to calculate Jaccard similarity
+def jaccard_similarity(set1, set2):
+    intersection = len(set1.intersection(set2))
+    union = len(set1.union(set2))
+    return intersection / union if union != 0 else 0
     
-    x, y = np.random.choice(data_len, 2)
-    if x == y or (x, y) in chosen: continue
-    chosen.add((x, y))
-    
-    vector_x = signatures[x]
-    vector_y = signatures[y]    
-    candidate = 1 if (x, y) in candidate_pairs else 0
-    cosine = cosine_similarity([vector_x], [vector_y])[0][0]
-    
-    x_.append(x)
-    y_.append(y)
-    jaccard_.append(jaccard(set(vector_x), set(vector_y)))
-    cosine_.append(cosine)
-    candidate_.append(candidate)
-
- 
-
-# add a normalized cosine column for better alignment
-cos_min = min(cosine_)
-cos_max = max(cosine_)   
- 
-cosine_norm = (cosine_ - cos_min) / (cos_max - cos_min)
-
 def probability(s, r, b):
-    # s: similarity
-    # r: rows (per band)
-    # b: number of bands
-    return 1 - (1 - s**r)**b
-
-def normalize(x, x_min, x_max):
-    return (x - x_min) / (x_max - x_min)
+        # s: similarity
+        # r: rows (per band)
+        # b: number of bands
+        return 1 - (1 - s**r)**b
  
-r = int(100 / b)
-s_scores = np.arange(0.01, 1, 0.01)
-P_scores = [probability(s, r, b) for s in s_scores]
-
-plt.plot(s_scores, P_scores)
-plt.scatter(x=cosine_norm, y=candidate_, alpha=0.5)
-plt.ylabel('candidates')
-plt.xlabel('Similarity (cosine)')
-plt.show()
-
-def tune_parameters(b_max, r_max, s1=0.80, s2=0.60, target_recall=0.90, target_precision=0.05):
+def tune_parameters(b_max, r_max, s1=0.80, s2=0.60, target_recall=0.90, target_precision=0.05, n_hashes = 200): 
+    
     best_b = 0
     best_r = 0
     best_recall = 0
@@ -215,11 +90,6 @@ def tune_parameters(b_max, r_max, s1=0.80, s2=0.60, target_recall=0.90, target_p
                     best_precision = precision
 
     return best_b, best_r
-
-#b, r = tune_parameters(300, 1000)
-
-print("Best b:", b)
-print("Best r:", r)
  
 def calculate_jaccard_similarities(dataset):
     similarities = {}
@@ -252,7 +122,7 @@ def calculate_false_positives_and_negatives(jaccard_similarities, candidate_pair
 
     return false_positives, false_negatives
  
-def evaluate_lsh(b, r):
+def evaluate_lsh(b, r, articles, signatures):
     # Sample a subset of your dataset
     dataset_sample = articles[:1000] 
 
@@ -274,7 +144,217 @@ def evaluate_lsh(b, r):
     print("False positive percentage:", false_positive_percentage)
     print("False negative percentage:", false_negative_percentage)
 
-values = [(20, 10)]
 
-for v in values:
-    evaluate_lsh(v[0], v[1])
+def run_single_lsh(b, r): 
+    
+    total_lines = 1000
+    
+    total_pairs = math.comb(total_lines, 2) 
+    
+    n_hashes = 200  
+    
+    
+    # Getting Data 
+    
+    filepath = "covid_news_small.json"
+
+    data = pd.read_json(filepath, lines=True)
+    data = data["text"].to_list() 
+    articles = data[0:total_lines]  
+
+   
+   
+    # Shingling
+   
+    k = 8  # shingle size
+ 
+    #print("Building Shingles...")
+   
+    # build shingles
+    shingles = []
+    for sentence in articles:
+        shingles.append(build_shingles(sentence, k))
+        
+    #print("Building Vocab...")
+    # build vocab
+    vocab = build_vocab(shingles)
+
+    #print("One-Hot Encoding...")
+    # one-hot encode our shingles
+    shingles_1hot = []
+    for shingle_set in shingles:
+        if not any(shingle_set):
+            shingle_set.add('special')  # Add a special element
+        shingles_1hot.append(one_hot(shingle_set, vocab))
+    # stack into single numpy array
+    shingles_1hot = np.stack(shingles_1hot) 
+
+
+
+    # Min Hashing
+
+    #print("Min Hashing...")    
+
+    arr = minhash_arr(vocab, n_hashes)
+
+    signatures = []
+
+    for vector in shingles_1hot:
+        signatures.append(get_signature(arr, vector))
+
+    # merge signatures into single array
+    signatures = np.stack(signatures) 
+
+
+
+    # LSH 
+
+    #print("Locality-Sensitive Hashing (LSH)...")
+
+   
+
+    lsh = LSH(b, r)
+
+    for signature in signatures:
+        lsh.add_hash(signature)
+        
+        
+    candidate_pairs = lsh.check_candidates() 
+ 
+    # Function to calculate Jaccard similarity
+    def jaccard_similarity(set1, set2):
+        intersection = len(set1.intersection(set2))
+        union = len(set1.union(set2))
+        return intersection / union if union != 0 else 0
+    
+    
+
+    # Ploting
+
+    #print("Plotting...")
+
+    #plot_lsh(signatures, shingles_1hot, candidate_pairs, b, r, total_pairs) 
+ 
+    
+    return candidate_pairs, articles
+    
+def get_results(candidate_pairs, articles):
+    
+    # Test Values
+    
+    cond_1= False
+    cond_2= False
+    
+    similarities = []
+    cond_1_v = 0
+    cond_2_v = 0
+    
+    for pair in candidate_pairs:
+        x, y = pair  
+        
+        vector_x = articles[x]
+        vector_y = articles[y]    
+        sim = jaccard_similarity(set(vector_x), set(vector_y))
+        similarities.append(sim)
+
+        if sim > 0.85:
+            cond_1_v += 1
+        
+        if sim < 0.6:
+            cond_2_v += 1
+        
+        
+    cond_1_res = cond_1_v/len(candidate_pairs)
+    cond_2_res = cond_2_v/len(candidate_pairs)
+
+    print("")
+    print("Percentage of pairs with more than 85%","similarity: ", "%.3f" % (cond_1_res*100), "%")
+    print("Percentage of pairs with less than 60%","similarity: ", "%.3f" % (cond_2_res*100), "%\n") 
+    
+    if cond_1_res > 0.90:
+        cond_1 = True
+    
+    if cond_2_res < 0.05:
+        cond_2 = True
+     
+    return cond_1, cond_2
+
+if __name__ == '__main__':
+    
+    # Create the parser 
+    
+    parser = argparse.ArgumentParser(description='Process some integers.')
+
+    
+    
+    # Add the arguments
+    
+    parser.add_argument('-b', type=int, required=True, help='The value for b')
+    parser.add_argument('-r', type=int, required=True, help='The value for r')
+
+    
+    
+    # Parse the arguments
+    
+    args = parser.parse_args()
+    
+    b = args.b 
+    r = args.r
+    
+    # Run LSH
+    
+    #candidate_pairs, articles = run_single_lsh(b, r) 
+    
+    #cond_1, cond_2 = get_results(candidate_pairs, articles) 
+    
+    #print("Condition 1:", cond_1)
+    #print("Condition 2:", cond_2)
+    
+    values = ((20, 5), (20, 10), (50, 10), (50,  20), (100,  10),  (200,  10))
+    
+    for v in values:
+        print("\n\n --------------------------------------------------")
+        print("\nfor values: ", v)
+        
+        b, r = v  
+ 
+        candidate_pairs, articles = run_single_lsh(b, r) 
+    
+        cond_1, cond_2 = get_results(candidate_pairs, articles) 
+
+        
+        
+        print("Finds as candidates at least 90% of pairs with 85% similarity:", cond_1)
+        print("Finds as candidates less than 5% of pairs with 60% similarity:", cond_2)
+        
+        print("\n")
+     
+
+        # Calculate stuff
+        false_positives = 0
+        false_negatives = 0
+        i = 0
+        for (idx1, article1), (idx2, article2) in combinations(enumerate(articles), 2):
+            i+=1
+            similarity = jaccard_similarity(set(article1), set(article2))
+            
+            if similarity > 0.85 and (idx1, idx2) not in candidate_pairs:
+                false_negatives += 1    
+            elif similarity < 0.6 and (idx1, idx2) in candidate_pairs:
+                false_positives += 1 
+                 
+            
+                
+        # Calculate percentages
+        total_possitive = len(candidate_pairs)
+        total = i
+        
+        percentage_false_positives = (false_positives / total) * 100
+        percentage_false_negatives = (false_negatives / total) * 100
+
+        print("Percentage of false positives:", "%.3f" %  percentage_false_positives, "%")
+        print("Percentage of false negatives:", "%.3f" %  percentage_false_negatives, "%")
+        
+     
+    
+    
